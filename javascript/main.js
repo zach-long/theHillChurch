07d86ed7bfdb560f057054519da27dfde3124183
Zach <trombonezach@gmail.com>
2017-04-10 20:30:12 -0400

    Beginning of sticky nav

---
+++ javascript/main.js
@@ -0,0 +1,14 @@
+//$(document).ready(function() {
+  /*var viewField = $(window);
+  var stickyNav = $('#stickyNav');*/
+
+  console.log('ready');
+
+  /*viewField.scroll(function(event) {
+  	objBottom = $(stickyNav).offset().top + $(stickyNav).outerHeight();
+  	winBottom = viewField.scrollTop() + viewField.height();
+  	if(winBottom > objBottom && !$(stickyNav).hasClass('glued')) {
+  		$(stickyNav).addClass("glued");
+  	}
+  });*/
+//});
