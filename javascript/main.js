63255e150fff6dab1b021c4bc35f0b60e8bde850
Zach <trombonezach@gmail.com>
2017-04-14 12:30:40 -0400

    Styling, stickiness

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,25 @@
-//$(document).ready(function() {
-  /*var viewField = $(window);
-  var stickyNav = $('#stickyNav');*/
+$(document).ready(function() {
+  // initialize placeholders for variables to be manipulated
+  // - in a higher scope in case used elsewhere
+  var winTop;
+  var headerBottom;
+  var nav = $('#stickyNav');
 
-  console.log('ready');
+  // listen for scroll event
+  $(window).scroll(function(event) {
+    // update window position variable values on every scroll
+    winTop = $(window).scrollTop();
+    headerBottom = $('#header').offset().top + $('#header').outerHeight();
 
-  /*viewField.scroll(function(event) {
-  	objBottom = $(stickyNav).offset().top + $(stickyNav).outerHeight();
-  	winBottom = viewField.scrollTop() + viewField.height();
-  	if(winBottom > objBottom && !$(stickyNav).hasClass('glued')) {
-  		$(stickyNav).addClass("glued");
-  	}
-  });*/
-//});
+    // check if nav should become sticky and lose stickiness
+  	if(winTop - headerBottom > 0 && !$(nav).hasClass('glued')) {
+  		$(nav).addClass("glued");
+      $('body').addClass("glued-buffer");
+  	} else if (winTop - headerBottom < -50 && $(nav).hasClass('glued')) {
+      $(nav).removeClass("glued");
+      $('body').removeClass("glued-buffer");
+    } else {
+      return;
+    }
+  });
+});
