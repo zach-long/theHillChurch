a1b67ff17d4c1801bc3f2626c26afff9f983c6be
Zachary Long <zach.writes.code@gmail.com>
2017-04-27 19:12:51 -0400

    mobile navbar

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
+  var isMobile = false;
+
   // initialize placeholders for variables to be manipulated
   // - in a higher scope in case used elsewhere
   var winTop;
   var headerBottom;
   var nav = $('#stickyNav');
+  var toggle = $('#toggleNav');
+
+  // detect if mobile
+  if (/Android|webOS|iPhone|iPad|BlackBerry|Windows Phone|Opera Mini|IEMobile|Mobile/i.test(navigator.userAgent)) {
+	   isMobile = true;
+  }
+
+  // click handler for displaying hidden mobile nav
+  $('#toggleNav').click(function(event) {
+    if(!$('#stickyNav').hasClass('visible')) {
+      $('#stickyNav').addClass("visible");
+    } else {
+      $('#stickyNav').removeClass("visible");
+    }
+  });
 
   // listen for scroll event
   $(window).scroll(function(event) {
@@ -12,12 +29,14 @@ $(document).ready(function() {
     headerBottom = $('#header').offset().top + $('#header').outerHeight();
 
     // check if nav should become sticky and lose stickiness
-  	if(winTop - headerBottom > 0 && !$(nav).hasClass('glued')) {
-  		$(nav).addClass("glued");
+  	if(winTop - headerBottom > 0 && !nav.hasClass('glued')) {
+  		nav.addClass("glued");
       $('body').addClass("glued-buffer");
-  	} else if (winTop - headerBottom < -50 && $(nav).hasClass('glued')) {
-      $(nav).removeClass("glued");
+      toggle.addClass("nullify-toggle-margin");
+  	} else if (winTop - headerBottom < -50 && nav.hasClass('glued')) {
+      nav.removeClass("glued");
       $('body').removeClass("glued-buffer");
+      toggle.removeClass("nullify-toggle-margin");
     } else {
       return;
     }
